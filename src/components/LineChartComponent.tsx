0793771cdcc71c0ac869758209f90f88f1dee0ed
Yoshinori Kanno <yoshinori.kanno@me.com>
2024-05-10 00:43:56 +0900

    ✨ Highcharts でグラフを描画

--- src/components/LineChartComponent.tsx
+++ src/components/LineChartComponent.tsx
@@ -1,35 +1,37 @@
-// components/LineChartComponent.tsx
+// src/components/LineChartComponent.jsx
 import React from 'react';
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
 
-// データ型の定義
-interface ChartData {
-  name: string;
-  uv: number;
-  pv: number;
-  amt: number;
+import Highcharts from 'highcharts';
+import HighchartsAccessibility from 'highcharts/modules/accessibility';
+
+import HighchartsReact from 'highcharts-react-official';
+
+// モジュールの初期化
+HighchartsAccessibility(Highcharts);
+
+const LineChartComponent = () => {
+  const options = {
+    title: {
+      text: 'My chart'
+    },
+    series: [
+      {
+        data: [1, 2, 3, 4, 5, 6]
+      }
+    ],
+    accessibility: {
+      enabled: false
+    }
+  };
+
+  return (
+    <div>
+      <HighchartsReact
+        highcharts={Highcharts}
+        options={options}
+      />
+    </div>
+  );
 }
-
-// サンプルデータ
-const data: ChartData[] = [
-  { name: 'Page A', uv: 400, pv: 2400, amt: 2400 },
-  { name: 'Page B', uv: 300, pv: 1388, amt: 2200 },
-  { name: 'Page C', uv: 200, pv: 9800, amt: 2290 },
-  { name: 'Page D', uv: 278, pv: 3908, amt: 2000 },
-  { name: 'Page E', uv: 189, pv: 4800, amt: 2181 },
-  { name: 'Page F', uv: 239, pv: 3800, amt: 2500 },
-  { name: 'Page G', uv: 349, pv: 4300, amt: 2100 },
-];
-
-const LineChartComponent: React.FC = () => (
-  <LineChart width={400} height={400} data={data}>
-    <XAxis dataKey="name" />
-    <YAxis />
-    <CartesianGrid strokeDasharray="3 3" />
-    <Tooltip />
-    <Legend />
-    <Line type="monotone" dataKey="uv" stroke="#8884d8" />
-  </LineChart>
-);
 
 export default LineChartComponent;
